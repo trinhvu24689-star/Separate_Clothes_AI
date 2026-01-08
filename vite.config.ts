6c91c320a2901ed77644f2464fd04688662d8243
Quang Hổ <trinhvu24689@gmail.com>
2026-01-08 20:59:55 +0700

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  // Use the provided API Key if env.API_KEY is not set
-  const apiKey = env.API_KEY || 'AIzaSyBfQrr9WM5gfKQwyMhwwW2ScDeRrA3cpxE';
+  // Use the provided API Key if env.GEMINI_API_KEY is not set
+  const apiKey = env.GEMINI_API_KEY || 'AIzaSyBfQrr9WM5gfKQwyMhwwW2ScDeRrA3cpxE';
   
   return {
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(apiKey),
+      'process.env.GEMINI_API_KEY': JSON.stringify(apiKey),
       // Fix process is not defined error in browser
       'process.env': JSON.stringify(env) 
     },
